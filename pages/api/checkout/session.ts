abccd8d6c6904967b76fdc63cefd6dc0c80bbb5c
Kevin Luo <kvn.luo@gmail.com>
2020-12-14 21:40:08 -0500

    attempts to get 400 response

--- pages/api/checkout/session.ts
+++ pages/api/checkout/session.ts
@@ -6,14 +6,16 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
 });
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { quantity } = req.body;
   const session = await stripe.checkout.sessions.create({
     success_url: `${req.headers.origin}/result?session_id={CHECKOUT_SESSION_ID}`,
     cancel_url: `${req.headers.origin}/checkout`,
     payment_method_types: ['card'],
     line_items: [
-      { price: process.env.PRICE_ID, quantity: 1 },
+      { price: process.env.PRICE_ID, quantity },
     ],
     mode: 'payment',
   })
-  res.status(200).json({ name: "Kevin Luo" })
+  console.log(session.id);
+  res.status(200).json({ sessionId: session.id })
 }
--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,7 +1,33 @@
+import { loadStripe } from "@stripe/stripe-js"
+
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY)
+
 export default function Checkout() {
+  const handleClick = async () => {
+    const { sessionId } = await fetch("/api/checkout/session", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify({ quantity: 1 })
+    }).then(res => {
+      console.log(res);
+      return res.json()
+    })
+
+    const stripe = await stripePromise;
+
+    const result = await stripe.redirectToCheckout({
+      sessionId,
+    })
+    if (result.error) {
+      console.log(result.error.message);
+    }
+  }
+
   return (
-    <div>
-      <h1>Checkout</h1>
-    </div>
+    <button role="link" onClick={handleClick}>
+      Checkout
+    </button>
   )
 }
