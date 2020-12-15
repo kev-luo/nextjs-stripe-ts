ff53c1ece6cf865c23003e6f8c990a47715b8b5f
Kevin Luo <kvn.luo@gmail.com>
2020-12-14 19:02:42 -0500

    create stripe checkout session

--- pages/api/checkout/session.ts
+++ pages/api/checkout/session.ts
@@ -1,5 +1,19 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import Stripe from 'stripe';
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: '2020-08-27',
+});
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const session = await stripe.checkout.sessions.create({
+    success_url: `${req.headers.origin}/result?session_id={CHECKOUT_SESSION_ID}`,
+    cancel_url: `${req.headers.origin}/checkout`,
+    payment_method_types: ['card'],
+    line_items: [
+      { price: process.env.PRICE_ID, quantity: 1 },
+    ],
+    mode: 'payment',
+  })
   res.status(200).json({ name: "Kevin Luo" })
 }
